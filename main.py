68f90cc0407c2caaec0c964a150f958edfc5f242
ConnorHartland <hartlanddevelop@gmail.com>
2020-02-05 15:57:57 -0800

    Hello

---
+++ main.py
@@ -0,0 +1,30 @@
+def insertBoard(letter, pos):
+	pass
+
+def spaceIsFree(pos):
+	pass
+
+def isWinner(bo, le):
+	pass
+
+def playerMove():
+	pass
+
+def selectRandom(li):
+	pass
+
+def compMove():
+	pass
+
+def isBoardFull(board):
+	pass
+
+def printBoard():
+	pass
+
+def main():
+	pass
+
+board = [' ' for x in range(10)]
+
+print(board)
